0bd98cc8c936acac3ae8923e93c37bc51bf7c307
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-05 16:34:11 +0300

    Add SignalR Consumer Console for Receiving Alerts
    - Implemented SignalR consumer to connect, listen for alerts, and handle connection retries.
    - Added events for connection status and alert reception.
    
    This commit introduces a console app that processes real-time alerts from the SignalR hub.

---
+++ ServerMonitoringSystem.ConsumerClient/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        string signalRUrl = "https://localhost:7271/hub/alerts";
+        var consumer = new SignalRConsumer(signalRUrl);
+
+        consumer.OnConnected += () =>
+        {
+            Console.WriteLine("Connected to SignalR hub.");
+        };
+
+        consumer.OnRetrying += (attempt) =>
+        {
+            Console.WriteLine($"Connection failed. Retrying in 2 seconds... (Attempt {attempt})");
+        };
+
+        consumer.OnAlertReceived += (serverId, alertType, timestamp) =>
+        {
+            Console.WriteLine($"Received alert: {alertType} from {serverId} at {timestamp}");
+        };
+
+        await consumer.StartAsync();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+        await consumer.StopAsync();
+    }
+}
---
+++ ServerMonitoringSystem.ConsumerClient/SignalRConsumer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+public class SignalRConsumer
+{
+    private readonly HubConnection _connection;
+
+    public event Action? OnConnected;
+    public event Action<int>? OnRetrying;
+    public event Action<string, string, DateTime>? OnAlertReceived;
+
+    public SignalRConsumer(string signalRUrl)
+    {
+        _connection = new HubConnectionBuilder()
+            .WithUrl(signalRUrl)
+            .WithAutomaticReconnect()
+            .Build();
+    }
+
+    public async Task StartAsync()
+    {
+        var anomalyAlert = "ReceiveAlert";
+
+        _connection.On<string, string, DateTime>(anomalyAlert, (serverId, alertType, timestamp) =>
+        {
+            OnAlertReceived?.Invoke(serverId, alertType, timestamp);
+        });
+
+        var maxAttempts = 5;
+        var attempt = 0;
+
+        while (attempt < maxAttempts)
+        {
+            try
+            {
+                await _connection.StartAsync();
+                OnConnected?.Invoke();
+                break;
+            }
+            catch (HttpRequestException)
+            {
+                attempt++;
+                OnRetrying?.Invoke(attempt);
+                await Task.Delay(2000);
+            }
+        }
+
+        await Task.Delay(-1);
+    }
+
+
+    public async Task StopAsync()
+    {
+        await _connection.StopAsync();
+    }
+}
