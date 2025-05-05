0e2d54c1d8e4338369d912d145ffd2070823543b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-05 09:47:34 +0300

    Add configuration classes and appsettings for anomaly detection and SignalR.

--- ServerMonitoringSystem.MessageProcessor/Program.cs
+++ ServerMonitoringSystem.MessageProcessor/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using ServerMonitoringSystem.MessageProcessor.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var configuration = builder.Build();
+
+var anomalyConfig = configuration
+    .GetSection("AnomalyDetectionConfig")
+    .Get<AnomalyDetectionConfig>();
+
+var signalRConfig = configuration
+    .GetSection("SignalRConfig")
+    .Get<SignalRConfig>();
