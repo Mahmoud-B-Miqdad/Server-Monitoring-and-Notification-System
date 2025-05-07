8ef8ce538edcc341d7b98066f6fb6c1eea28b67b
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-07 17:45:48 +0300

    Fixed missing use of IStatisticsCollector interface in MonitoringService constructor for improved testability and mocking in unit tests.

--- ServerMonitoringSystem/Services/MonitoringService.cs
+++ ServerMonitoringSystem/Services/MonitoringService.cs
@@ -6,11 +6,11 @@ namespace ServerMonitoringSystem.Services;
 
 public class MonitoringService : IMonitoringService
 {
-    private readonly StatisticsCollector _collector;
+    private readonly IStatisticsCollector _collector;
     private readonly IMessagePublisher _publisher;
     private readonly ServerStatisticsConfig _config;
 
-    public MonitoringService(StatisticsCollector collector, IMessagePublisher publisher, ServerStatisticsConfig config)
+    public MonitoringService(IStatisticsCollector collector, IMessagePublisher publisher, ServerStatisticsConfig config)
     {
         _collector = collector;
         _publisher = publisher;
