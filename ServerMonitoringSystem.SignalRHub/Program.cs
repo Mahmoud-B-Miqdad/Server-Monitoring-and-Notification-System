bcee291e7d477ebe50fd45c6097892a7120280a7
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-08 09:39:20 +0300

    Cleaned up unused controller and Swagger configurations from SignalR Hub project.

--- ServerMonitoringSystem.SignalRHub/Program.cs
+++ ServerMonitoringSystem.SignalRHub/Program.cs
@@ -3,24 +3,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseRouting();
-app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllers();
     endpoints.MapHub<AlertHub>("/hub/alerts");
 });
 
